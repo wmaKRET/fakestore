642e34461c726d1c3f3050d430e46ba735a84790
wmaKRET <wkret91.2@gmail.com>
2022-08-08 11:51:31 +0200

    Cleanup Heder component

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,17 +1,19 @@
-import { BsCartFill } from "react-icons/bs"
+import { BsCartFill } from "react-icons/bs";
 import "./header.scss";
 
 const Header = () => {
   return (
     <header className="header">
-      <a className="header__logo" href="#">FakeStore</a>
+      <a className="header__logo" href="#">
+        FakeStore
+      </a>
       <div className="header__sections">
         <div>Men</div>
         <div>Women</div>
         <div>Jewelery</div>
       </div>
       <div className="header__cart">
-        <BsCartFill size={20}/>
+        <BsCartFill size={20} />
       </div>
     </header>
   );
